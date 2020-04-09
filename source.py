fcb08c9a0dda1fedd2dffd543a9f6830220e51fd
pvl <mateescupavel@yahoo.com>
2020-04-09 20:28:53 +0300

    Added an example

---
+++ source.py
@@ -0,0 +1,18 @@
+import gi
+gi.require_version('Notify', '0.7')
+from gi.repository import Notify
+Notify.init("Test")
+
+import time
+
+
+while True:
+    notification = Notify.Notification.new(
+        "Notificarea ta din 20 in 20 de secunde", 
+        "┗(＾0＾)┓"   
+    )
+    notification.set_urgency(0)
+    notification.show()
+
+    time.sleep(20)    
+
