2013635dcf3e504d2f7d351dc58cfa96eab9f939
pvl <mateescupavel@yahoo.com>
2020-04-18 00:23:24 +0300

    Modified the source file to be run as a script

--- source.py
+++ source.py
@@ -1,19 +1,25 @@
+#!/usr/bin/env python3
+
 import gi
 gi.require_version('Notify', '0.7')
 from gi.repository import Notify
-Notify.init("Test")
 
 import time
 
+def main():
+    Notify.init("Test")
 
-while True:
-    notification = Notify.Notification.new(
-        "Notificarea ta din 20 in 20 de secunde", 
-        "┗(＾0＾)┓"   
-    )
-    notification.set_urgency(0)
-    notification.show()
+    while True:
+        notification = Notify.Notification.new(
+            "Notificarea ta din 20 in 20 de secunde", 
+            "┗(＾0＾)┓"   
+        )
+        notification.set_urgency(0)
+        notification.show()
 
-    time.sleep(20)    
+        time.sleep(20)    
 
-Notify.uninit("Test")
+    Notify.uninit("Test")
+
+if __name__ == "__main__":
+    main()
